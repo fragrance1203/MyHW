d151b30780451b26a78191f4bf3bb25992bbef4a
fragrance1203 <gustn1203@naver.com>
2018-06-16 23:45:53 +0900

    first version

---
+++ src/B.java
@@ -0,0 +1,11 @@
+public class B { 
+ 	public static void main(String[] args) { 
+ 		int[] arr new int[10];
+		for(int i =0; i<arr.length; i++)
+		{
+		arr[i]=i+1;
+		}
+
+		System.out.println(arr[5]);
+ 	} 
+ } 
---
+++ src/C.java
@@ -0,0 +1,10 @@
+public class C { 
+ 	public static void main(String[] args) { 
+ 		
+		String str = "abcde";
+		String result1 = str.substring(3);
+		String result2 = str.substring(2,4);
+		System.out.println(result1);
+		System.out.println(result2);
+ 	} 
+ } 
